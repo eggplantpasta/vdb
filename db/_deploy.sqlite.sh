147e56729d29df8a5ac4c2cb61258c72458087ba
Brett Roper <roper.brett@gmail.com>
2024-05-17 08:39:44 +0800

    Add example data

--- db/_deploy.sqlite.sh
+++ db/_deploy.sqlite.sh
@@ -9,10 +9,12 @@ sqlite3 vdb.db <<EOF
 .read service_history.create.sql
 
 -- insert seed data
-
 .import --csv --skip 1 'specifications.csv' specifications
 
 -- import example data
+.import --csv --skip 1 'vehicle.example.csv' vehicle
+.import --csv --skip 1 'service_schedule.example.csv' service_schedule
+.import --csv --skip 1 'service_history.example.csv' service_history
 
 .quit
 EOF
